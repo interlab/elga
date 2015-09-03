0182d942d91fd48c037b528893b48a16f5699651
inter <pmrtorrents@gmail.com>
2015-09-03 09:19:24 +0300

    admin hooks
    Signed-off-by: inter <pmrtorrents@gmail.com>

--- Elga.subs.php
+++ Elga.subs.php
@@ -38,6 +38,7 @@ function elga_menu_buttons(&$buttons, &$menu_count)
     ], 'after');
 }
 
+// integrate_whos_online
 function elga_whos_online($actions)
 {
     global $scripturl, $txt;
@@ -93,6 +94,86 @@ function elga_whos_online($actions)
 
     if (!empty($action))
         return $action; # !important
+}
+
+// integrate_admin_areas
+// Menu.subs.php - 87
+function elga_admin_areas(&$admin_areas)
+{
+    global $txt;
+
+    // loadLanguage('AdminElga');
+    // loadLanguage('HelpElga');
+    $txt['gallery_title'] = 'Галерея';
+
+    // $admin_areas['config']['areas']['modsettings']['subsections']['elga'] = [$txt['gallery_title']];
+	$admin_areas['config']['areas']['addonsettings']['subsections']['elga'] = [$txt['gallery_title']];
+}
+
+// integrate_sa_modify_modifications
+function elga_sa_modify_modifications(&$subActions)
+{
+    $subActions['elga'] = [
+		'dir' => SUBSDIR,
+		'file' => 'Elga.subs.php',
+		'function' => 'elga_addon_settings',
+		'permission' => 'admin_forum',
+	];
+}
+
+function elga_addon_settings()
+{
+	global $txt, $context, $scripturl, $modSettings;
+
+	// loadlanguage('Elga');
+    $txt['elga_title'] = 'Gallery Settings';
+    $txt['elga_desc'] = 'This addon adds a images gallery.';
+    $txt['elga_enabled'] = 'Enable Gallery';
+    $txt['elga_enabled_desc'] = '';
+    $txt['elga_max_width_img'] = 'Максимальная ширина изображения';
+    $txt['elga_max_height_img'] = 'Максимальная высота изображения';
+    
+	$context[$context['admin_menu_name']]['tab_data']['tabs']['elga']['description'] = $txt['elga_desc'];
+
+	// Lets build a settings form
+	require_once(SUBSDIR . '/SettingsForm.class.php');
+
+	// Instantiate the form
+	$elgaSettings = new Settings_Form();
+
+	// All the options, well at least some of them!
+	$config_vars = [
+		['check', 'elga_enabled', 'postinput' => $txt['fancybox_enabled_desc']],
+        [ 'int', 'elga_max_width_img', ],
+        [ 'int', 'elga_max_height_img', ],
+	];
+
+	// Load the settings to the form class
+	$elgaSettings->settings($config_vars);
+
+	// Saving?
+	if (isset($_GET['save']))
+	{
+		checkSession();
+
+		// Some defaults are good to have
+		if (empty($_POST['elga_max_width_img']))
+			$_POST['elga_max_width_img'] = 350;
+		if (empty($_POST['elga_max_height_img']))
+			$_POST['elga_max_height_img'] = 350;
+
+		Settings_Form::save_db($config_vars);
+		redirectexit('action=admin;area=addonsettings;sa=elga');
+	}
+
+	// Continue on to the settings template
+	$context['page_title'] = $context['settings_title'] = $txt['elga_title'];
+	$context['post_url'] = $scripturl . '?action=admin;area=addonsettings;sa=elga;save';
+
+	// if (!empty($modSettings['ююю']))
+		// updateSettings(array('ююю' => 'ююю'));
+
+	Settings_Form::prepare_db($config_vars);
 }
 
 
